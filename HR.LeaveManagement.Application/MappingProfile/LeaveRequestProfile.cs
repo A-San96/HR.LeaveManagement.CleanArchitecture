fc904730be245c1217c3ebc4a6996b9975400558
A-San96 <assanediatta96@gmail.com>
2023-06-17 20:28:07 +0000

    feat: add files and folders structure for Leave Request features

---
+++ HR.LeaveManagement.Application/MappingProfile/LeaveRequestProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.MappingProfile;
+
+internal class LeaveRequestProfile : Profile
+{
+    public LeaveRequestProfile()
+    {
+        CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        CreateMap<LeaveType, LeaveTypeDetailsDto>();
+        CreateMap<CreateLeaveTypeCommand, LeaveType>();
+        CreateMap<UpdateLeaveTypeCommand, LeaveType>();
+    }
+}
